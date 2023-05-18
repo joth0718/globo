5a5328cfcf441d4d4af76b6ee5738a4e7c4adadd
joth0718 <joth0718@gmail.com>
2023-05-18 17:13:04 +0200

    add hooks for add, delete, update house

--- reactweb/src/hooks/HouseHooks.ts
+++ reactweb/src/hooks/HouseHooks.ts
@@ -1,7 +1,8 @@
 import { House } from "../types/house";
 import Config from "../config";
-import { useQuery } from "react-query";
-import axios, { AxiosError } from "axios";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { useNavigate } from "react-router-dom";
 
 const useFetchHouses = () => {
     return useQuery<House[], AxiosError>("houses", () =>
@@ -14,5 +15,50 @@ const useFetchHouse = (id: number) => {
     axios.get(`${Config.baseApiUrl}/house/${id}`).then((resp) => resp.data));
 }
 
+const useAddHouse = () => {
+    const nav =    useNavigate();
+    const queryClient = useQueryClient();
+    return  useMutation<AxiosResponse, AxiosError, House>(
+        (h) => axios.post(`${Config.baseApiUrl}/houses`,h),
+        {
+            onSuccess: () => 
+            {
+                queryClient.invalidateQueries("houses");
+                nav("/")
+            }
+        }
+    ); 
+};
+
+const useUpdateHouse = () => {
+    const nav =    useNavigate();
+    const queryClient = useQueryClient();
+    return  useMutation<AxiosResponse, AxiosError, House>(
+        (h) => axios.put(`${Config.baseApiUrl}/houses`,h),
+        {
+            onSuccess: (_, house) => 
+            {
+                queryClient.invalidateQueries("houses");
+                nav(`/house/${house.id}`);
+            }
+        }
+    ); 
+};
+
+const useDeleteHouse = () => {
+    const nav =    useNavigate();
+    const queryClient = useQueryClient();
+    return  useMutation<AxiosResponse, AxiosError, House>(
+        (h) => axios.delete(`${Config.baseApiUrl}/houses/${h.id}`),
+        {
+            onSuccess: () => 
+            {
+                queryClient.invalidateQueries("houses");
+                nav("/")
+            }
+        }
+    ); 
+};
+
 export default useFetchHouses;
-export {useFetchHouse};
+export {useFetchHouse, useAddHouse, useUpdateHouse, useDeleteHouse};
